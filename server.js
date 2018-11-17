3f3f40561846c043a71cb45d3bf3e3ef0e1125b6
AndrewIRobles <40839081+AndrewIRobles@users.noreply.github.com>
2018-11-17 14:47:08 -0600

    AR added server.js file

--- server.js
+++ server.js
@@ -1,21 +1,47 @@
+require("dotenv").config();
 var express = require("express");
-var PORT = process.env.PORT || 8080;
-var app = express();
-
-app.use(express.static("public"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+var db = require("./models");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controllers");
+// Routes
+require("./routes/api-Routes")(app);
+require("./routes/html-Routes")(app);
 
-app.use(routes);
+var syncOptions = { force: false };
 
-app.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:" + PORT);
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
+
+module.exports = app;
