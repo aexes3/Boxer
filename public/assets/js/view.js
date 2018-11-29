4df49acdb1b302bae53f3c0a42577976b50c63b2
Wilmer <39922930+JAWilmer@users.noreply.github.com>
2018-11-28 22:57:03 -0600

    Add files via upload

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,43 +1,40 @@
 $(document).ready(function() {
   // Getting a reference to the input field where user adds a new product
-  var $newItemInput = $("input.new-item");
+  // var $newItemInput = $("input.new-item");
   // Our new products will go inside the productContainer
-  var $productContainer = $(".product-container");
+  let smBox4 = $(".smBox4");
   // Adding event listeners for deleting, editing, and adding products
  
   $(document).on("click", "button.complete", toggleComplete);
   $(document).on("click", ".product-item", editProduct);
   $(document).on("keyup", ".product-item", finishEdit);
   $(document).on("blur", ".product-item", cancelEdit);
-  $(document).on("submit", "#product-form", insertProduct);
-  // $(document).on("click", "button.etsy", insertEtsy);
-
-  // Our initial products array
- var products;
+  
+  // Initial products array
 
   // Getting products from database when page loads
   getProducts();
 
   // This function resets the products displayed with new products from the database
-  function initializeRows() {
-    $productContainer.empty();
+  function initializeRows(products) {
+    smBox4.empty();
     var rowsToAdd = [];
     for (var i = 0; i < products.length; i++) {
       rowsToAdd.push(createNewRow(products[i]));
     }
-    $productContainer.prepend(rowsToAdd);
+    smBox4.prepend(rowsToAdd);
   }
 
   // This function grabs products from the database and updates the view
   function getProducts() {
-    $.get("/api/products", function(data) {
-      console.log(data);
-      products=data;
-        // for (var i = 0; i < data.length; i++) {
+    $.get("/api/products", function(products) {
+      console.log(products);
+        
+      // for (var i = 0; i < data.length; i++) {
         //   console.log(data[i].title);
-        //   $(".product-container").append("<p>" + data[i].title + "</p>");
+        //   $(".smBox4").append("<p>" + data[i].title + "</p>");
         // }
-      initializeRows();
+      initializeRows(products);
     });
   }
 
@@ -49,18 +46,6 @@ $(document).ready(function() {
     $(this).children("input.edit").show();
     $(this).children("input.edit").focus();
   }
-// // JW guess at how to add radio buttons
-//   function editProduct() {
-//     var currentProduct = $(this).data("product");
-//     $(this).children().hide();
-//     $(this).children("input.edit").val(currentProduct.title);
-//     $(this).children("input.edit").show();
-//     $(this).children("input.edit").focus();
-//   }
-
-//   sub this for "input.edit"
-//  '<input type="radio" id=' + post.title + " " + 'value=' + post.title + " " + '><label for='+ post.title + " " + '</label>'
-  
 
   // Toggles complete status
   function toggleComplete(event) {
@@ -107,40 +92,21 @@ $(document).ready(function() {
     console.log("THIS WORKS!product", product.title);
     console.log("THIS WORKS!ID", product.id);
     var $newInputRow = $(
-      [
-        "<li class='list-group-item product-item'>",
-        "<span>",
-        product.title,
-        "</span>",
-        "<input type='text' class='edit' style='display: none;'>",
-        
-        "<button class='complete btn btn-primary'>✓</button>",
-        "</li>"
-      ].join("")
+    `<input type="radio" id="${product.title}" value="${product.title}">
+      <label for="${product.title}">${product.title}</label>
+    </input>`
     );
+    // mdn article radio button to select only one
 
     $newInputRow.find("input.edit").css("display", "none");
     $newInputRow.data("product", product);
-    if (product.complete) {
-      $newInputRow.find("span").css("text-decoration", "line-through");
-      console.log("product:", product);
+    // if (product.complete) {
+    //   $newInputRow.find("span").css("text-decoration", "line-through");
+    //   console.log("product:", product);
 
-    }
+    // }
     return $newInputRow;
   }
-
-  
-// This function inserts a new product into our database and then updates the view
-function insertProduct(event) {
-  event.preventDefault();
-  var product = {
-    text: $newItemInput.val().trim(),
-    complete: false
-  };
-
-  $.post("/api/products", product, getProducts);
-  $newItemInput.val("");
-}
 });
 
 
