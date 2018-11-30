bf059462bc36e29b3c7f23ffafb1c487b8888f0e
Wilmer <39922930+JAWilmer@users.noreply.github.com>
2018-11-30 17:18:55 -0600

    Add files via upload

--- public/js/lgCustomBox.js
+++ public/js/lgCustomBox.js
@@ -49,12 +49,13 @@ $(document).ready(function() {
     // Constructs a product-item row
 function createNewRow(product) {
   var $newInputRow = $(
-    `<input type="radio" name="4" id="${product.product_ID}" 
+    `<ul><li><input type="radio" name="4" id="${product.product_ID}" 
   value="${product.product_ID}">
   <label for="${product.product_ID}">
   <a href="#" data-toggle="popover" title="${product.title}">
-  <img src="${product.imageURL}" margin="2%" width="175" height="175">
-  </img></a></label></input>`
+  <img src="${product.imageURL}" width="175" height="175">
+    </img></a></label></input><div class="check"></div></li>
+    </ul>`
   );
   $newInputRow.data("product", product);
   return $newInputRow;
@@ -109,38 +110,39 @@ function createNewRow(product) {
 // Constructs a product-item row
 function createNewRow(product) {
   var $newInputRow = $(
-    `<input type="radio" name="4" id="${product.product_ID}" 
+    `<ul><li><input type="radio" name="5" id="${product.product_ID}"  
   value="${product.product_ID}">
   <label for="${product.product_ID}">
   <a href="#" data-toggle="popover" title="${product.title}">
-  <img src="${product.imageURL}" margin="2%" width="175" height="175">
-  </img></a></label></input>`
+  <img src="${product.imageURL}" width="175" height="175">
+        </img></a></label></input><div class="check"></div></li>
+        </ul>`
   );
   $newInputRow.data("product", product);
   return $newInputRow;
 }
 }
 
-  // LARGE BOX: Creates $8 column
-  function createLgBox8() {
-    let lgBox8 = $(".lgBox8");
+  // LARGE BOX: Creates $9 column
+  function createLgBox9() {
+    let lgBox9 = $(".lgBox9");
 
     // Get products from database when page loads
     getProducts();
 
     // Resets the products displayed with new products from the database
     function initializeRows(products) {
-      lgBox8.empty();
+      lgBox9.empty();
       var rowsToAdd = [];
       for (var i = 0; i < 5; i++) {
         rowsToAdd.push(createNewRow(products[i]));
       }
-      lgBox8.prepend(rowsToAdd);
+      lgBox9.prepend(rowsToAdd);
     }
 
     // Grabs products from the database and updates the view
     function getProducts() {
-      $.get("/api/8products", function(products) {
+      $.get("/api/9products", function(products) {
         initializeRows(products);
       });
     }
@@ -169,12 +171,13 @@ function createNewRow(product) {
     // Constructs a product-item row
     function createNewRow(product) {
       var $newInputRow = $(
-        `<input type="radio" name="4" id="${product.product_ID}" 
+        `<ul><li><input type="radio" name="9" id="${product.product_ID}"  
       value="${product.product_ID}">
       <label for="${product.product_ID}">
       <a href="#" data-toggle="popover" title="${product.title}">
-      <img src="${product.imageURL}" margin="2%" width="175" height="175">
-      </img></a></label></input>`
+      <img src="${product.imageURL}" width="175" height="175">
+        </img></a></label></input><div class="check"></div></li>
+        </ul>`
       );
       $newInputRow.data("product", product);
       return $newInputRow;
@@ -229,12 +232,13 @@ function createNewRow(product) {
     // Constructs a product-item row
     function createNewRow(product) {
       var $newInputRow = $(
-        `<input type="radio" name="4" id="${product.product_ID}" 
+        `<ul><li><input type="radio" name="14" id="${product.product_ID}" 
       value="${product.product_ID}">
       <label for="${product.product_ID}">
       <a href="#" data-toggle="popover" title="${product.title}">
-      <img src="${product.imageURL}" margin="2%" width="175" height="175">
-      </img></a></label></input>`
+      <img src="${product.imageURL}" width="175" height="175">
+        </img></a></label></input><div class="check"></div></li>
+        </ul>`
       );
       $newInputRow.data("product", product);
       return $newInputRow;
@@ -243,7 +247,7 @@ function createNewRow(product) {
 
   createLgBox4();
   createLgBox5();
-  createLgBox8();
+  createLgBox9();
   createLgBox14();
 
   let form = document.querySelector("#lgBoxForm");
@@ -257,7 +261,7 @@ function createNewRow(product) {
       for (const entry of data) {
         // output = entry[0] + " = " + entry[1] + "\r";
         for (var i = 0; i < entry.length; i++) {
-          console.log("entry[i]", entry[i]);
+          
           console.log(entry);
         }
       }
